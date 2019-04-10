2b2825c6c72eeac7f21a3f92c702c6fee7ebcdfe
cdbm <cdbm@cin.ufpe.br>
2019-04-09 21:48:23 -0300

    adicionando exemplos e simplificando o código

--- test/app/test.py
+++ test/app/test.py
@@ -20,23 +20,13 @@ def upload():
         print("Couldn't create upload directory: {}".format(target))
     print(request.files.getlist("file"))
     for upload in request.files.getlist("file"):
-        print(upload)
-        print("{} is the file name".format(upload.filename))
         filename = upload.filename
         destination = "/".join([target, filename])
-        print ("Accept incoming file:", filename)
-        print ("Save it to:", destination)
         upload.save(destination)
         with open(destination) as f:
             data = json.load(f)
         print(data)
-
-    # return send_from_directory("images", filename, as_attachment=True)
     return render_template("complete.html", data=data)
-
-#@app.route('/upload/<filename>')
-#def send_image(filename):
-#   return send_from_directory("images", filename)
 
 if __name__ == "__main__":
     app.run( debug=True)
